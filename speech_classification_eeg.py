676097148c634f0ef9dd1d6f5fec7facb28565b1
NIRVANALAN <yushi.lan@outlook.com>
2023-01-10 22:32:11 +0800

    train pipeline init

--- speech_classification_eeg.py
+++ speech_classification_eeg.py
@@ -1,42 +1,16 @@
-"""
-Speech Command Classification with torchaudio
-*********************************************
-This tutorial will show you how to correctly format an audio dataset and
-then train/test an audio classifier network on the dataset.
-Colab has GPU option available. In the menu tabs, select “Runtime” then
-“Change runtime type”. In the pop-up that follows, you can choose GPU.
-After the change, your runtime should automatically restart (which means
-information from executed cells disappear).
-First, let’s import the common torch packages such as
-`torchaudio <https://github.com/pytorch/audio>`__ that can be installed
-by following the instructions on the website.
-"""
+import sys
+from pathlib import Path
+from pdb import set_trace as st
 
-# Uncomment the line corresponding to your "runtime type" to run in Google Colab
-
-# CPU:
-# !pip install pydub torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
-
-# GPU:
-# !pip install pydub torch==1.7.0+cu101 torchvision==0.8.1+cu101 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
-
+import matplotlib.pyplot as plt
+import scipy
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchaudio
-import sys
-
-import matplotlib.pyplot as plt
-import IPython.display as ipd
-
 from tqdm import tqdm
-
-
-######################################################################
-# Let’s check if a CUDA GPU is available and select our device. Running
-# the network on a GPU will greatly decrease the training/testing runtime.
-#
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -64,34 +38,51 @@ print(device)
 # standard training, validation, testing subsets.
 #
 
-from torchaudio.datasets import SPEECHCOMMANDS
+# from torchaudio.datasets import SPEECHCOMMANDS
 import os
 
 
-class SubsetSC(SPEECHCOMMANDS):
-    def __init__(self, subset: str = None):
-        super().__init__("./", download=True)
+class EEGSubset(torch.utils.data.Dataset):
+    def __init__(self):
+        dataset_root = Path('/mnt/lustre/OneDrive/sensetime-server-mirror/EEG-Data/Preprocessed-upload') / 'Stand'
+        tinn_samples = ['AS_tinn_500and5kHz_mcattn-2_sess1_set1_ICAREV.mat']
+        cntl_samples= ['BH_cntl_2-Stream_mcattn-2_sess1_set1_ICAREV.mat']
+        self.waves = []
 
-        def load_list(filename):
-            filepath = os.path.join(self._path, filename)
-            with open(filepath) as fileobj:
-                return [os.path.normpath(os.path.join(self._path, line.strip())) for line in fileobj]
+        for sample_name in tinn_samples:
+            sample_mat = scipy.io.loadmat(str(dataset_root / sample_name))
+            allepochs = sample_mat['allepochs'][0,0].astype(np.float32) # TODO 360, 358, 64
+            allepochs = allepochs.transpose(0, 2, 1) # 360, 64, 358
+            playvecs = sample_mat['playvecs'][0,0] # 1, 360
+            # st()
+            for i in range(allepochs.shape[0]):
+                self.waves.append((allepochs[i], playvecs[0, i], 'tinn', 1))
 
-        if subset == "validation":
-            self._walker = load_list("validation_list.txt")
-        elif subset == "testing":
-            self._walker = load_list("testing_list.txt")
-        elif subset == "training":
-            excludes = load_list("validation_list.txt") + load_list("testing_list.txt")
-            excludes = set(excludes)
-            self._walker = [w for w in self._walker if w not in excludes]
+
+        for sample_name in cntl_samples:
+            sample_mat = scipy.io.loadmat(str(dataset_root / sample_name))
+            allepochs = sample_mat['allepochs'][0,0].astype(np.float32) # TODO
+            allepochs = allepochs.transpose(0, 2, 1) # 360, 64, 358
+            playvecs = sample_mat['playvecs'][0,0]
+            for i in range(allepochs.shape[0]):
+                self.waves.append((allepochs[i], playvecs[0, i], 'cntl', 0))
+
+        # todo, preprocess data into independent dataset. high-low-passive; names; 360/540 files.
+        # here, just load into the memory for testing
+    def __len__(self):
+        return len(self.waves)
+    
+    def __getitem__(self, idx):
+        return self.waves[idx]
+
 
 
 # Create training and testing split of the data. We do not use validation in this tutorial.
-train_set = SubsetSC("training")
-test_set = SubsetSC("testing")
+train_set = EEGSubset()
+print(f'dataset size: {len(train_set)}')
+# test_set = SubsetSC("testing")
 
-waveform, sample_rate, label, speaker_id, utterance_number = train_set[0]
+waveform, playvec, group, label = train_set[0] # 1*358*64, 1/2/3/4, cntl/tinn, 0/1
 
 
 ######################################################################
@@ -100,40 +91,19 @@ waveform, sample_rate, label, speaker_id, utterance_number = train_set[0]
 # the speaker, the number of the utterance.
 #
 
-print("Shape of waveform: {}".format(waveform.size()))
-print("Sample rate of waveform: {}".format(sample_rate))
+print("Shape of waveform: {}".format(waveform.shape)) # 1, 16k
+# print("Sample rate of waveform: {}".format(sample_rate))
 
-# plt.plot(waveform.t().numpy());
 
 
 ######################################################################
 # Let’s find the list of labels available in the dataset.
 #
 
-print('loading labels ')
-labels = sorted(list(set(datapoint[2] for datapoint in train_set)))
-print('loading labels done')
+# print('loading labels ')
+# labels = sorted(list(set(datapoint[2] for datapoint in train_set)))
+# print('loading labels done')
 # labels
-
-
-######################################################################
-# The 35 audio labels are commands that are said by users. The first few
-# files are people saying “marvin”.
-#
-
-# waveform_first, *_ = train_set[0]
-# ipd.Audio(waveform_first.numpy(), rate=sample_rate)
-
-# waveform_second, *_ = train_set[1]
-# ipd.Audio(waveform_second.numpy(), rate=sample_rate)
-
-
-######################################################################
-# The last file is someone saying “visual”.
-#
-
-# waveform_last, *_ = train_set[-1]
-# ipd.Audio(waveform_last.numpy(), rate=sample_rate)
 
 
 ######################################################################
@@ -151,34 +121,9 @@ print('loading labels done')
 # single channel for audio, this is not needed here.
 #
 
-new_sample_rate = 8000
-transform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=new_sample_rate)
-transformed = transform(waveform)
-
-# ipd.Audio(transformed.numpy(), rate=new_sample_rate)
-
-
-######################################################################
-# We are encoding each word using its index in the list of labels.
-#
-
-
-def label_to_index(word):
-    # Return the position of the word in labels
-    return torch.tensor(labels.index(word))
-
-
-def index_to_label(index):
-    # Return the word corresponding to the index in labels
-    # This is the inverse of label_to_index
-    return labels[index]
-
-
-word_start = "yes"
-index = label_to_index(word_start)
-word_recovered = index_to_label(index)
-
-print(word_start, "-->", index, "-->", word_recovered)
+# new_sample_rate = 8000
+# transform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=new_sample_rate)
+# transformed = transform(waveform)
 
 
 ######################################################################
@@ -194,33 +139,34 @@ print(word_start, "-->", index, "-->", word_recovered)
 #
 
 
-def pad_sequence(batch):
-    # Make all tensor in a batch the same length by padding with zeros
-    batch = [item.t() for item in batch]
-    batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0.)
-    return batch.permute(0, 2, 1)
+# def pad_sequence(batch):
+#     # Make all tensor in a batch the same length by padding with zeros
+#     batch = [item.t() for item in batch]
+#     batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0.)
+#     return batch.permute(0, 2, 1)
 
 
-def collate_fn(batch):
+# def collate_fn(batch):
 
-    # A data tuple has the form:
-    # waveform, sample_rate, label, speaker_id, utterance_number
+#     # A data tuple has the form:
+#     # waveform, sample_rate, label, speaker_id, utterance_number
 
-    tensors, targets = [], []
+#     tensors, targets = [], []
 
-    # Gather in lists, and encode labels as indices
-    for waveform, _, label, *_ in batch:
-        tensors += [waveform]
-        targets += [label_to_index(label)]
+#     # Gather in lists, and encode labels as indices
+#     for waveform, _, label, *_ in batch:
+#         tensors += [waveform]
+#         targets += [label_to_index(label)]
 
-    # Group the list of tensors into a batched tensor
-    tensors = pad_sequence(tensors)
-    targets = torch.stack(targets)
+#     # Group the list of tensors into a batched tensor
+#     tensors = pad_sequence(tensors)
+#     targets = torch.stack(targets)
 
-    return tensors, targets
+#     return tensors, targets
 
 
-batch_size = 256
+# batch_size = 256
+batch_size = 32
 
 if device == "cuda":
     num_workers = 1
@@ -233,19 +179,20 @@ train_loader = torch.utils.data.DataLoader(
     train_set,
     batch_size=batch_size,
     shuffle=True,
-    collate_fn=collate_fn,
+    # collate_fn=collate_fn,
     num_workers=num_workers,
     pin_memory=pin_memory,
 )
-test_loader = torch.utils.data.DataLoader(
-    test_set,
-    batch_size=batch_size,
-    shuffle=False,
-    drop_last=False,
-    collate_fn=collate_fn,
-    num_workers=num_workers,
-    pin_memory=pin_memory,
-)
+test_loader = train_loader
+# test_loader = torch.utils.data.DataLoader(
+#     test_set,
+#     batch_size=batch_size,
+#     shuffle=False,
+#     drop_last=False,
+#     collate_fn=collate_fn,
+#     num_workers=num_workers,
+#     pin_memory=pin_memory,
+# )
 
 
 ######################################################################
@@ -267,33 +214,45 @@ test_loader = torch.utils.data.DataLoader(
 
 
 class M5(nn.Module):
-    def __init__(self, n_input=1, n_output=35, stride=16, n_channel=32):
+    def __init__(self, n_input=1, n_output=35, stride=1, n_channel=32):
         super().__init__()
-        self.conv1 = nn.Conv1d(n_input, n_channel, kernel_size=80, stride=stride)
+        # self.conv1 = nn.Conv1d(n_input, n_channel, kernel_size=80, stride=stride)
+        self.conv1 = nn.Conv1d(n_input, n_channel, kernel_size=16, stride=stride) # 358 / 20 ~= 18, receptive field = 20ms
         self.bn1 = nn.BatchNorm1d(n_channel)
-        self.pool1 = nn.MaxPool1d(4)
+        # self.pool1 = nn.MaxPool1d(4)
+        self.pool1 = nn.MaxPool1d(2)
         self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
         self.bn2 = nn.BatchNorm1d(n_channel)
-        self.pool2 = nn.MaxPool1d(4)
+        # self.pool2 = nn.MaxPool1d(4)
+        self.pool2 = nn.MaxPool1d(2)
         self.conv3 = nn.Conv1d(n_channel, 2 * n_channel, kernel_size=3)
         self.bn3 = nn.BatchNorm1d(2 * n_channel)
-        self.pool3 = nn.MaxPool1d(4)
+        # self.pool3 = nn.MaxPool1d(4)
+        self.pool3 = nn.MaxPool1d(2)
         self.conv4 = nn.Conv1d(2 * n_channel, 2 * n_channel, kernel_size=3)
         self.bn4 = nn.BatchNorm1d(2 * n_channel)
-        self.pool4 = nn.MaxPool1d(4)
+        # self.pool4 = nn.MaxPool1d(4)
+        self.pool4 = nn.MaxPool1d(2)
         self.fc1 = nn.Linear(2 * n_channel, n_output)
 
     def forward(self, x):
         x = self.conv1(x)
+        # print(x.shape)
         x = F.relu(self.bn1(x))
         x = self.pool1(x)
+        # print(x.shape)
         x = self.conv2(x)
+        # print(x.shape)
         x = F.relu(self.bn2(x))
         x = self.pool2(x)
+        # print(x.shape)
         x = self.conv3(x)
+        # print(x.shape)
         x = F.relu(self.bn3(x))
         x = self.pool3(x)
+        # print(x.shape)
         x = self.conv4(x)
+        # print(x.shape)
         x = F.relu(self.bn4(x))
         x = self.pool4(x)
         x = F.avg_pool1d(x, x.shape[-1])
@@ -302,7 +261,8 @@ class M5(nn.Module):
         return F.log_softmax(x, dim=2)
 
 
-model = M5(n_input=transformed.shape[0], n_output=len(labels))
+# TODO, tune the parameters
+model = M5(n_input=waveform.shape[0], n_output=2, n_channel=32) # binary classification 
 model.to(device)
 print(model)
 
@@ -340,13 +300,13 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)  # red
 
 def train(model, epoch, log_interval):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    for batch_idx, (data, playvec, group,  target) in enumerate(train_loader):
 
         data = data.to(device)
         target = target.to(device)
 
         # apply transform and model on whole batch directly on device
-        data = transform(data)
+        # data = transform(data) # TODO?
         output = model(data)
 
         # negative log-likelihood for a tensor of size (batch x 1 x n_output)
@@ -389,13 +349,13 @@ def get_likely_index(tensor):
 def test(model, epoch):
     model.eval()
     correct = 0
-    for data, target in test_loader:
+    for data,_, _, target in test_loader:
 
         data = data.to(device)
         target = target.to(device)
 
         # apply transform and model on whole batch directly on device
-        data = transform(data)
+        # data = transform(data)
         output = model(data)
 
         pred = get_likely_index(output)
@@ -415,13 +375,13 @@ def test(model, epoch):
 #
 
 log_interval = 20
-n_epoch = 2
+n_epoch = 50
 
 pbar_update = 1 / (len(train_loader) + len(test_loader))
 losses = []
 
 # The transform needs to live on the same device as the model and the data.
-transform = transform.to(device)
+# transform = transform.to(device)
 with tqdm(total=n_epoch) as pbar:
     for epoch in range(1, n_epoch + 1):
         train(model, epoch, log_interval)
@@ -440,37 +400,37 @@ with tqdm(total=n_epoch) as pbar:
 #
 
 
-def predict(tensor):
-    # Use the model to predict the label of the waveform
-    tensor = tensor.to(device)
-    tensor = transform(tensor)
-    tensor = model(tensor.unsqueeze(0))
-    tensor = get_likely_index(tensor)
-    tensor = index_to_label(tensor.squeeze())
-    return tensor
+# def predict(tensor):
+#     # Use the model to predict the label of the waveform
+#     tensor = tensor.to(device)
+#     tensor = transform(tensor)
+#     tensor = model(tensor.unsqueeze(0))
+#     tensor = get_likely_index(tensor)
+#     tensor = index_to_label(tensor.squeeze())
+#     return tensor
 
 
-waveform, sample_rate, utterance, *_ = train_set[-1]
-ipd.Audio(waveform.numpy(), rate=sample_rate)
+# waveform, sample_rate, utterance, *_ = train_set[-1]
+# ipd.Audio(waveform.numpy(), rate=sample_rate)
 
-print(f"Expected: {utterance}. Predicted: {predict(waveform)}.")
+# print(f"Expected: {utterance}. Predicted: {predict(waveform)}.")
 
 
 ######################################################################
 # Let’s find an example that isn’t classified correctly, if there is one.
 #
 
-for i, (waveform, sample_rate, utterance, *_) in enumerate(test_set):
-    output = predict(waveform)
-    if output != utterance:
-        ipd.Audio(waveform.numpy(), rate=sample_rate)
-        print(f"Data point #{i}. Expected: {utterance}. Predicted: {output}.")
-        break
-else:
-    print("All examples in this dataset were correctly classified!")
-    print("In this case, let's just look at the last data point")
-    ipd.Audio(waveform.numpy(), rate=sample_rate)
-    print(f"Data point #{i}. Expected: {utterance}. Predicted: {output}.")
+# for i, (waveform, sample_rate, utterance, *_) in enumerate(test_set):
+#     output = predict(waveform)
+#     if output != utterance:
+#         ipd.Audio(waveform.numpy(), rate=sample_rate)
+#         print(f"Data point #{i}. Expected: {utterance}. Predicted: {output}.")
+#         break
+# else:
+#     print("All examples in this dataset were correctly classified!")
+#     print("In this case, let's just look at the last data point")
+#     ipd.Audio(waveform.numpy(), rate=sample_rate)
+#     print(f"Data point #{i}. Expected: {utterance}. Predicted: {output}.")
 
 
 ######################################################################
@@ -480,54 +440,47 @@ else:
 #
 
 
-def record(seconds=1):
+# def record(seconds=1):
 
-    from google.colab import output as colab_output
-    from base64 import b64decode
-    from io import BytesIO
-    from pydub import AudioSegment
+#     from google.colab import output as colab_output
+#     from base64 import b64decode
+#     from io import BytesIO
+#     from pydub import AudioSegment
 
-    RECORD = (
-        b"const sleep  = time => new Promise(resolve => setTimeout(resolve, time))\n"
-        b"const b2text = blob => new Promise(resolve => {\n"
-        b"  const reader = new FileReader()\n"
-        b"  reader.onloadend = e => resolve(e.srcElement.result)\n"
-        b"  reader.readAsDataURL(blob)\n"
-        b"})\n"
-        b"var record = time => new Promise(async resolve => {\n"
-        b"  stream = await navigator.mediaDevices.getUserMedia({ audio: true })\n"
-        b"  recorder = new MediaRecorder(stream)\n"
-        b"  chunks = []\n"
-        b"  recorder.ondataavailable = e => chunks.push(e.data)\n"
-        b"  recorder.start()\n"
-        b"  await sleep(time)\n"
-        b"  recorder.onstop = async ()=>{\n"
-        b"    blob = new Blob(chunks)\n"
-        b"    text = await b2text(blob)\n"
-        b"    resolve(text)\n"
-        b"  }\n"
-        b"  recorder.stop()\n"
-        b"})"
-    )
-    RECORD = RECORD.decode("ascii")
+#     RECORD = (
+#         b"const sleep  = time => new Promise(resolve => setTimeout(resolve, time))\n"
+#         b"const b2text = blob => new Promise(resolve => {\n"
+#         b"  const reader = new FileReader()\n"
+#         b"  reader.onloadend = e => resolve(e.srcElement.result)\n"
+#         b"  reader.readAsDataURL(blob)\n"
+#         b"})\n"
+#         b"var record = time => new Promise(async resolve => {\n"
+#         b"  stream = await navigator.mediaDevices.getUserMedia({ audio: true })\n"
+#         b"  recorder = new MediaRecorder(stream)\n"
+#         b"  chunks = []\n"
+#         b"  recorder.ondataavailable = e => chunks.push(e.data)\n"
+#         b"  recorder.start()\n"
+#         b"  await sleep(time)\n"
+#         b"  recorder.onstop = async ()=>{\n"
+#         b"    blob = new Blob(chunks)\n"
+#         b"    text = await b2text(blob)\n"
+#         b"    resolve(text)\n"
+#         b"  }\n"
+#         b"  recorder.stop()\n"
+#         b"})"
+#     )
+#     RECORD = RECORD.decode("ascii")
 
-    print(f"Recording started for {seconds} seconds.")
-    display(ipd.Javascript(RECORD))
-    s = colab_output.eval_js("record(%d)" % (seconds * 1000))
-    print("Recording ended.")
-    b = b64decode(s.split(",")[1])
+#     print(f"Recording started for {seconds} seconds.")
+#     display(ipd.Javascript(RECORD))
+#     s = colab_output.eval_js("record(%d)" % (seconds * 1000))
+#     print("Recording ended.")
+#     b = b64decode(s.split(",")[1])
 
-    fileformat = "wav"
-    filename = f"_audio.{fileformat}"
-    AudioSegment.from_file(BytesIO(b)).export(filename, format=fileformat)
-    return torchaudio.load(filename)
-
-
-# Detect whether notebook runs in google colab
-if "google.colab" in sys.modules:
-    waveform, sample_rate = record()
-    print(f"Predicted: {predict(waveform)}.")
-    ipd.Audio(waveform.numpy(), rate=sample_rate)
+#     fileformat = "wav"
+#     filename = f"_audio.{fileformat}"
+#     AudioSegment.from_file(BytesIO(b)).export(filename, format=fileformat)
+#     return torchaudio.load(filename)
 
 
 ######################################################################
